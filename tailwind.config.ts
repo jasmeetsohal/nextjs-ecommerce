27e3c8f3499d1a1c52267b2090f9584f67fe8399
jasmeetsohal <jasmeetsohal93@gmail.com>
2024-03-02 00:29:04 +0530

    chore: add assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,10 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
@@ -37,4 +27,3 @@ const config: Config = {
     ],
   },
 };
-export default config;
