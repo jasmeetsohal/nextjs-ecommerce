941b5bc1a7d187fe8902dd16e2c312dfa8a9546c
jasmeetsohal <jasmeetsohal93@gmail.com>
2024-03-04 00:58:15 +0530

    feat: add product list page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [{ hostname: "images.unsplash.com" },{hostname:"plus.unsplash.com"}],
+    },
+    experimental: {
+      serverActions: true,
+    },
+  };
+  
+  export default nextConfig;
---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,67 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+    title: "Add Product - Flowmazon"
+}
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageurl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+
+  if (!name || !description || !imageUrl || !price) {
+    throw new Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+     data: {name,description,imageUrl,price}
+    
+  });
+
+  redirect("/");
+}
+
+export default function AddProductPage() {
+    return (
+        <div>
+            <h1 className="text-lg mb-3 font-bold">Add Product</h1>
+            <form action={addProduct}>
+                <input
+                   required
+                    name="name"
+                    placeholder="Name"
+                   className="input-bordered input mb-3 w-full"
+                />
+                <textarea
+                required
+                name="description"
+                placeholder="Description"
+                className="textarea textarea-bordered mb-3 w-full"
+                />
+                <input
+                   required
+                    name="imageurl"
+                    placeholder="Image URL"
+                    type="url"
+                   className="input-bordered input mb-3 w-full"
+                />
+                <input
+                   required
+                    name="price"
+                    placeholder="Price"
+                    type="number"
+                   className="input-bordered input mb-3 w-full"
+                />
+                <FormSubmitButton className="btn-block">
+                    Add Product
+                </FormSubmitButton>
+            </form>
+        </div>
+    );
+}
